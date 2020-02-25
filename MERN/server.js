da6d4d3a19c57882272f1310302a98a61c8bd216
nandakishore323 <51471551+nandakishore323@users.noreply.github.com>
2020-02-25 15:15:06 +0530

    Updated Server.js for Authorization

--- MERN/server.js
+++ MERN/server.js
@@ -1,22 +1,37 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
+const MongoClient = require('mongodb').MongoClient
+
+const mongoose = require('mongoose');
+
+// Initializing Express
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-// run mongod in terminal before connecting to mongodb locally
-const MONGODB_URI = "mongodb://localhost/shopping";
+// Use Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-    })
-    .then(() => console.log('Mongoose is connected'))
-    .catch(err => console.log('Error :' + err))
-;
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server Started on port : ${port}`)); 
+
+const url = 'mongodb://127.0.0.1:27017/E-Marketplace'
+
+mongoose.connect(url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    })
+
+const db = mongoose.connection
+db.once('open', _ => {
+  console.log('Database connected:', url)
+})
+
+db.on('error', err => {
+  console.error('connection error:', err)
+})
+
